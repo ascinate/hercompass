4c0be06bf3c214fc9adec10a2b81f82382749e48
ski12345491 <abu@ascinatetech.com>
2025-11-21 18:22:50 +0530

    testing1234

--- src/app.js
+++ src/app.js
@@ -27,7 +27,18 @@ app.use(
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://progressive-hercompass.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
