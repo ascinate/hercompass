e565ffa2f03281e1659c4f646ab7c58401dc49b3
abu-saied-mondal <abumondal990@gmail.com>
2025-11-22 15:14:12 +0530

    first committttt

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ app.use(
       "https://progressive-hercompass.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
